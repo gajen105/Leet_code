57c96ab6c741738a1f76ab2275c476ab6a7be355
gajen105 <gjndrrajpurohit@gmail.com>
2025-03-31 23:22:48 +0530

    1071

---
+++ Leet/1071. Greatest Common Divisor of Strings.py
@@ -0,0 +1,30 @@
+# 1071. Greatest Common Divisor of Strings
+# For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t 
+# (i.e., t is concatenated with itself one or more times).
+
+# Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+class Solution(object):
+    def gcdOfStrings(self, str1, str2):
+        """
+        :type str1: str
+        :type str2: str
+        :rtype: str
+        """
+        l1 = len(str1)
+        l2 = len(str2)
+        def is_gcd(str):
+            return (str1 == str*(l1//len(str)) and str2 == str*(l2//len(str)))
+        if str1+str2 != str2+str1:
+            return ""
+        l_min = min(l1,l2)
+        for i in range(l_min,0,-1):
+            print(i)
+            if l1%(i)==0 and l2%(i)==0 :
+                if is_gcd(str1[:i]):
+                    return str1[:i]
+        return ""
+    
+str1 ="AAAA"
+str2 ="AAA"
+sol = Solution()
+print(sol.gcdOfStrings(str1, str2))
